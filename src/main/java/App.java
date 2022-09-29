c4a28a7db1a70dc53e438bedae28b5d45e31f45d
Yuriy Lihanov <cool.lihanov@yandex.ru>
2022-09-29 16:16:21 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beeen =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beeen.getMassage());
+        Cat beeen1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beeen1.getMassage());
+
+        System.out.println(bean == bean1);
+        System.out.println(beeen == beeen1);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMassage("Cattttttttttt");
+        return cat;
+    }
 }
